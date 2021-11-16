5931608db7143fe722eef1c70214c0b5b2506219
Mark Momot <markmomot03@gmail.com>
2021-11-15 19:06:18 -0500

    commit tree 28f8e2c8601bd64d2f0d02365310f47a5d4cc81f

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const categoryElements = categories.map((category, index) => (<button key={index} className="" onClick = {e => handleFilter(e)} >{category}</button>))
+
+  function handleFilter(e){
+    e.target.className = "selected"
+
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryElements}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  const tasksElements = tasks.map(task => <Task category={task.category} text={task.text} key={task.text}/>)
+
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksElements}
     </div>
   );
 }
